13dc49a29db2b49d80ea0ad6e16e5a6b666b8df0
a2814218 <danielrayzel@alunos.utfpr.edu.br>
2025-09-03 11:42:08 -0300

    Create fazer lista 2 exercicicio5.c

---
+++ fazer lista 2 exercicicio5.c
@@ -0,0 +1,46 @@
+int main() {
+    
+ 
+
+srand(time(0));
+
+
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+int main() {
+    
+int numeroqualquer, i, n;
+
+srand(time(0));
+
+int numeroaleatorio = rand() % 500 + 1;
+
+
+
+   n= numerodetentativas
+   
+    printf("Número aleatório gerado entre 1 e 500: %d\n", numeroaleatorio);
+    
+    if numerodetentativas
+    
+    for(i=1; i<n ; i++)  {
+    printf("digite um numeroqualquer");
+    scanf("%d" , &numeroqualquer);
+    
+    if numeroqualquer < numeroaleatorio{
+    printf("digite outro numero");
+    }
+    else if numeroqualquer> numeroaleatorio{
+    printf("digite outro numero");
+    }
+    else {
+    printf(" acertou o numero aleatorio");
+    }
+  
+}
+    return 0;
+}
+
